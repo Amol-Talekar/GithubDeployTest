70a8b7b6651453900a8fb21d5689377d76be5448
Amol-Talekar <amol11talekar@gmail.com>
2022-10-12 14:05:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashLink } from 'react-router-hash-link';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbarClass'>
+        <HashLink to="#bikes" smooth className='hashlinkClass'>Bikes</HashLink>
+        <HashLink to="#cars" smooth className='hashlinkClass'>Cars</HashLink>
+        <HashLink to="#boats" smooth className='hashlinkClass'>Boats</HashLink>
+        <HashLink to="#planes" smooth className='hashlinkClass'>Planes</HashLink>
+      </div>
+
+      <div id='bikes' className='all'>
+          <h4>Bike</h4>
+         <img src='https://imgd.aeplcdn.com/1280x720/n/cw/ec/43482/sp-125-right-front-three-quarter-2.jpeg?isig=0&q=80' alt='bike' />
+      </div>
+
+      <div id='cars' className='all'>
+         <h4>Cars</h4>
+         <img src='https://imgd.aeplcdn.com/1056x594/n/cw/ec/123185/grand-vitara-exterior-right-front-three-quarter-2.jpeg?isig=0&q=75&wm=1' alt='car' />
+      </div>
+
+      <div id='boats' className='all'>
+        <h4>Boats</h4> 
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Amerigo_vespucci_1976_nyc_aufgetakelt.jpg/1280px-Amerigo_vespucci_1976_nyc_aufgetakelt.jpg' alt='boat' />
+      </div>
+
+      <div id='planes' className='all'>
+         <h4>Plane</h4>
+         <img src='https://upload.wikimedia.org/wikipedia/commons/5/5e/ANA_777-300_Taking_off_from_JFK.jpg' alt='plane' />
+      </div>
     </div>
   );
 }
